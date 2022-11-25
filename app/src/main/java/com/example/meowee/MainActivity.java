69252bbda5a470934168abefb3c78a5c25ab66b2
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2022-11-26 02:04:00 +0700

    Add custom bottom navigation bar.

--- app/src/main/java/com/example/meowee/MainActivity.java
+++ app/src/main/java/com/example/meowee/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.meowee;
 
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -27,13 +30,55 @@ public class MainActivity extends AppCompatActivity {
     public static User currentUser;
     public static DatabaseReference currentUserDatabaseRef;
 
-    private TextView hello;
+    // UI Elements
+    private ImageButton buttonHome, buttonMap, buttonCamera, buttonFavorite, buttonProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        hello = findViewById(R.id.hello);
+
+        buttonHome = findViewById(R.id.bottomNavBarButtonHome);
+        buttonMap = findViewById(R.id.bottomNavBarButtonMap);
+        buttonCamera = findViewById(R.id.bottomNavBarButtonCamera);
+        buttonFavorite = findViewById(R.id.bottomNavBarButtonFavorite);
+        buttonProfile = findViewById(R.id.bottomNavBarButtonProfile);
+
+        buttonHome.setOnClickListener(onBottomNavBarButtonClicked);
+        buttonMap.setOnClickListener(onBottomNavBarButtonClicked);
+        buttonFavorite.setOnClickListener(onBottomNavBarButtonClicked);
+        buttonProfile.setOnClickListener(onBottomNavBarButtonClicked);
+
+    }
+
+    private final View.OnClickListener onBottomNavBarButtonClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            resetSelectedBottomNavbarButton();
+            int viewId = view.getId();
+            if (viewId == R.id.bottomNavBarButtonHome) {
+                Log.d(TAG, "Home button clicked!");
+                buttonHome.setImageResource(R.drawable.home_selected);
+            } else if (viewId == R.id.bottomNavBarButtonMap) {
+                Log.d(TAG, "Map button clicked!");
+                buttonMap.setImageResource(R.drawable.map_selected);
+            } else if (viewId == R.id.bottomNavBarButtonFavorite) {
+                Log.d(TAG, "Favorite button clicked!");
+                buttonFavorite.setImageResource(R.drawable.favorite_selected);
+            } else if (viewId == R.id.bottomNavBarButtonProfile) {
+                Log.d(TAG, "Profile button clicked!");
+                buttonProfile.setImageResource(R.drawable.profile_selected);
+            } else {
+                Log.d(TAG, "Don't know which button clicked!");
+            }
+        }
+    };
+
+    private void resetSelectedBottomNavbarButton() {
+        buttonHome.setImageResource(R.drawable.home_unselected);
+        buttonMap.setImageResource(R.drawable.map_unselected);
+        buttonFavorite.setImageResource(R.drawable.favorite_unselected);
+        buttonProfile.setImageResource(R.drawable.profile_unselected);
     }
 
     @Override
@@ -76,6 +121,6 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private void updateUI() {
-        hello.setText(String.format("Hi, %s", currentUser.getFullName()));
+        // TODO: Update greeting to new name
     }
 }
